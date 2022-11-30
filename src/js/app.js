b73b175806cd9e597dcc108e012ec060efb2d2f9
Plamenpowa <pkostadinov72@gmail.com>
2022-11-30 03:55:13 +0200

    adding fire emoji

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const prices = document.querySelectorAll(".price");
+  for (let price of prices) {
+    if (price.classList.contains("hot")) {
+      price.textContent += "🔥";
+    }
+  }
 });
